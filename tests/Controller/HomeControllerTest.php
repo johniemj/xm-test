839a12bdb39bd586c6867059e3fdce96d5ea16ce
j_maris <jm.live.net@gmail.com>
2019-09-22 10:30:20 +0300

    Edit some messages

--- tests/Controller/HomeControllerTest.php
+++ tests/Controller/HomeControllerTest.php
@@ -7,40 +7,44 @@ use Symfony\Bundle\FrameworkBundle\Test\WebTestCase;
 class HomeControllerTest extends WebTestCase
 {
     public function testIndex()
-    {
-        $client = static::createClient();
+    {   
+      $client = static::createClient();
 
-        $crawler = $client->request('GET', '/');
-        
-        $form = $crawler->selectButton('Search')->form();
+      $crawler = $client->request('GET', '/');
+      
+      $form = $crawler->selectButton('Search')->form();
 
-        $form['symbol_form[companySymbol]'] = 'AAPL';
-        $form['symbol_form[startDate]'] = '2010-08-01';
-        $form['symbol_form[endDate]'] = '2010-09-01';
-        $form['symbol_form[email]'] = 'john_maris@hotmail.com';
+      $form['symbol_form[companySymbol]'] = 'AAPL';
+      $form['symbol_form[startDate]'] = '2010-08-01';
+      $form['symbol_form[endDate]'] = '2010-09-01';
+      $form['symbol_form[email]'] = 'john_maris@hotmail.com';
 
-        $crawler = $client->submit($form);
+      $crawler = $client->submit($form);
 
-        $this->assertEquals(200, $client->getResponse()->getStatusCode());
+      $this->assertEquals(200, 
+                    $client->getResponse()->getStatusCode(),
+                    "Failed to submint the form");
     }
 
     public function testIndexWithEmailSend()
-    {
-        $client = static::createClient();
+    {   
+      $client = static::createClient();
 
-        $crawler = $client->request('GET', '/');
-        
-        $form = $crawler->selectButton('Search')->form();
+      $crawler = $client->request('GET', '/');
+      
+      $form = $crawler->selectButton('Search')->form();
 
-        $form['symbol_form[companySymbol]'] = 'AAPL';
-        $form['symbol_form[startDate]'] = '2010-08-01';
-        $form['symbol_form[endDate]'] = '2010-09-01';
-        $form['symbol_form[email]'] = 'john_maris@hotmail.com';
-        $form['symbol_form[emailSend]'] = '1';
+      $form['symbol_form[companySymbol]'] = 'AAPL';
+      $form['symbol_form[startDate]'] = '2010-08-01';
+      $form['symbol_form[endDate]'] = '2010-09-01';
+      $form['symbol_form[email]'] = 'john_maris@hotmail.com';
+      $form['symbol_form[emailSend]'] = '1';
 
-        $crawler = $client->submit($form);
-        
-        $this->assertEquals(200, $client->getResponse()->getStatusCode());
+      $crawler = $client->submit($form);
+      
+      $this->assertEquals(200, 
+                    $client->getResponse()->getStatusCode(),
+                    "Failed to call the form with send email checkbox");
     }
 
     public function testSearchResults()
@@ -80,7 +84,9 @@ class HomeControllerTest extends WebTestCase
         {          
           $concatenatedColumns .= $column->nodeValue;
         }
-        $this->assertEquals(implode($values), $concatenatedColumns);
+        $this->assertEquals(implode($values), 
+                            $concatenatedColumns,
+                          "The historical values are not correct or missing");
       }     
     }
 }
